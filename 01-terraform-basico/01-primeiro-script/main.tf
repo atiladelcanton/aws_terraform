7c247a19bc5e9c583e01deff8d3432dd32b0ebf5
Atila Rampazo <atilarampazo@gmail.com>
2023-02-21 09:50:24 -0300

    first script create simple s3 bucket

---
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "dev"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-atila-lindao"
+  tags = {
+    Name        = "My Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
