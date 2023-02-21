6f0e13ff29a19c119a6d434df614a07f1039a8b2
Atila Rampazo <atilarampazo@gmail.com>
2023-02-21 11:58:09 -0300

    Remote State

---
+++ 02-terraform-intermediario/01-usando-remote-state/backend.hcl
@@ -0,0 +1,4 @@
+bucket  = "tfstate-305204455969"
+key     = "dev/01-usando-remote-state/terraform.tfstate"
+region  = "us-east-1"
+profile = "dev"
---
+++ 02-terraform-intermediario/01-usando-remote-state/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+
+  backend "s3" {}
+
+
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
